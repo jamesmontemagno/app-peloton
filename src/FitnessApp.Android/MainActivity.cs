f4ff8faad7aa1104e4eb6c414f52e5a92fedcbca
James Montemagno <james.montemagno@gmail.com>
2021-01-22 11:36:01 -0800

    Add icons, start profile page, and fix challenges page with TabView

--- src/FitnessApp.Android/MainActivity.cs
+++ src/FitnessApp.Android/MainActivity.cs
@@ -9,7 +9,10 @@ using Android.OS;
 
 namespace FitnessApp.Droid
 {
-    [Activity(Label = "FitnessApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "FitnessApp",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ src/FitnessApp/Views/Profile/ProfileWorkoutsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessApp.Views.Profile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfileWorkoutsPage : ContentPage
+    {
+        public ProfileWorkoutsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

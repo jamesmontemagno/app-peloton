aa635c4861a27aff86711fd95ad3b7fb9d5496d1
James Montemagno <james.montemagno@gmail.com>
2021-02-12 09:55:15 -0800

    Add in Profile Workouts Page :)

---
+++ src/FitnessApp/Models/FinishedWorkout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessApp.Models
+{
+    public class FinishedWorkout
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Instructor { get; set; }
+        public string ActivityType { get; set; }
+        public string FinishedDate { get; set; }
+        public string AiredDate { get; set; }
+        public string RideLocation { get; set; }
+    }
+}
---
+++ src/FitnessApp/ViewModels/ProfileWorkoutsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.ViewModels
+{
+    public class ProfileWorkoutsViewModel : BaseViewModel
+    {
+
+        public AsyncCommand WorkoutOptionsCommand { get; }
+        public ProfileWorkoutsViewModel()
+        {
+            WorkoutOptionsCommand = new AsyncCommand(WorkoutOptions);
+        }
+
+        async Task WorkoutOptions()
+        {
+            await App.Current.MainPage.DisplayActionSheet("", "Cancel", "Delete Workout", "View Class Details", "Share Workout");
+        }
+    }
+}
